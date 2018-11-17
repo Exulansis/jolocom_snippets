689499d9fbf784843c155ecce711b3a3aa35ee70
Exulansis <rusu.eugeniu@gmx.de>
2018-11-17 16:50:27 +0100

    Finished the snippet update

--- snippets/3_credential_offer.ts
+++ snippets/3_credential_offer.ts
@@ -4,32 +4,30 @@ import { data } from './data'
 import { JSONWebToken } from 'jolocom-lib/js/interactionTokens/JSONWebToken'
 import { CredentialOffer } from 'jolocom-lib/js/interactionTokens/credentialOffer'
 import { ICredentialOfferAttrs } from 'jolocom-lib/js/interactionTokens/interactionTokens.types'
-import { CredentialResponse } from 'jolocom-lib/js/interactionTokens/credentialResponse'
 
 const reg = JolocomLib.registries.jolocom.create()
 const vault = new JolocomLib.keyProvider(data.seed, data.secret)
+const derivationArgs = {
+  derivationPath: JolocomLib.KeyTypes.jolocomIdentityKey,
+  encryptionPass: data.secret
+}
 
-reg
-  .authenticate(vault, { derivationPath: JolocomLib.KeyTypes.jolocomIdentityKey, encryptionPass: data.secret })
-  .then(async identityWallet => {
-    const { token } = await fetch(data.credentialOfferEndpoint).then(res => res.json())
+reg.authenticate(vault, derivationArgs).then(async identityWallet => {
+  const { token } = await fetch(data.credentialOfferEndpoint).then(res => res.json())
+  const parsed: JSONWebToken<CredentialOffer> = await JolocomLib.parse.interactionToken.fromJWT(token)
 
-    const parsed: JSONWebToken<CredentialOffer> = await JolocomLib.parse.interactionToken.fromJWT(token)
+  const credentialOfferResponse = await identityWallet.create.interactionTokens.response.offer(
+    parsed.interactionToken.toJSON() as ICredentialOfferAttrs,
+    data.secret,
+    parsed
+  )
 
-    const credentialOfferResponse = await identityWallet.create.interactionTokens.response.offer(
-      parsed.interactionToken.toJSON() as ICredentialOfferAttrs,
-      data.secret,
-      parsed
-    )
+  const encodedCredentialToken = await fetch(data.credentialReceiveEndpoint, {
+    method: 'POST',
+    body: JSON.stringify({ token: credentialOfferResponse.encode() }),
+    headers: { 'Content-Type': 'application/json' }
+  }).then(res => res.json())
 
-    const encodedCredentialToken = await fetch(data.credentialReceiveEndpoint, {
-      method: 'POST',
-      body: JSON.stringify({ token: credentialOfferResponse.encode() }),
-      headers: { 'Content-Type': 'application/json' }
-    }).then(res => res.json())
-    const receivedCredential: JSONWebToken<CredentialResponse> = await JolocomLib.parse.interactionToken.fromJWT(
-      encodedCredentialToken.token
-    )
-
-    console.log(receivedCredential.interactionToken.suppliedCredentials)
-  })
+  const receivedCredential = await JolocomLib.parse.interactionToken.fromJWT(encodedCredentialToken.token)
+  console.log(receivedCredential.interactionToken.signedCredentials)
+})
